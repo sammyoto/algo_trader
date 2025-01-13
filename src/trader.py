42593afda5c10a98783728eb3e58488d86a45d4b
sammyoto <samgfales@gmail.com>
2025-01-12 17:56:45 -0700

    added comments, fixed saving state

--- src/trader.py
+++ src/trader.py
@@ -9,52 +9,53 @@ from google.cloud import storage
 
 class Trader():
     def __init__(self, socketio, ticker, app_key, secret_key, callback_url = "https://127.0.0.1", debug = True):
-        self.ticker = ticker
-        self.debug = debug
+        # Set logging level
+        logging.basicConfig(level=logging.INFO)
 
-        self.last_action_price = TwoDecimal("0")
+        self.socketio: SocketIO = socketio # Socketio, necessary for sending updates to frontend
+        self.ticker = ticker # The ticker we want to trade
+        self.debug = debug # Set for debug mode (no real trades will be made in account)
+
+        # Start all values at 0 for debug, in prod load previous values via load_state
+        self.last_action_price = TwoDecimal("0") # Last price for buy or sell
+        self.market_price = TwoDecimal("0") # Current market price
+        self.last_price = TwoDecimal("0") # Previous market price
+        self.last_pivot = TwoDecimal("0") # Last price the stock price pivoted at
+        self.last_action = "hold" # Last action taken by trader
+        self.trend = "none" # Trend of the stock price (positive or negative)
+        self.session_profit = TwoDecimal("0") # Profit for the current trading session
+        self.system_message = "Nothing for now!" # Message for frontend
+
         # Load saved state from bucket only if deployed live
         if not self.debug:
             self.load_state("trader-bucket-61423", "state.json", "state.json")
 
-        self.market_price = TwoDecimal("0")
-        self.last_price = TwoDecimal("0")
-        self.last_pivot = TwoDecimal("0")
-        self.last_action = "hold"
-        self.trend = "none"
-        self.session_profit = TwoDecimal("0")
-        self.system_message = "Nothing for now!"
-
-        self.socketio: SocketIO = socketio
-
-        # set logging level
-        logging.basicConfig(level=logging.INFO)
-
         # For schwabdev client
-        self.client = schwabdev.Client(app_key, secret_key, callback_url)  #create a client
-        self.linked_accounts = self.client.account_linked().json() # get linked account
-        self.account_hash = self.linked_accounts[0].get('hashValue') # get account hash
-        self.api_handler = API_Handler(self.client, self.account_hash) # create API handler
+        self.client = schwabdev.Client(app_key, secret_key, callback_url) # Create a client
+        self.linked_accounts = self.client.account_linked().json() # Get linked account
+        self.account_hash = self.linked_accounts[0].get('hashValue') # Get account hash
+        self.api_handler = API_Handler(self.client, self.account_hash) # Create API handler
 
-        # get from account
+        # Get data from account
         account_data = self.api_handler.get_account_data()
-        self.account_cash = TwoDecimal(account_data["account_balance"])
-        self.current_holdings = TwoDecimal(account_data["current_holdings"])
+        self.account_cash = TwoDecimal(account_data["account_balance"]) # Current cash in account
+        self.current_holdings = TwoDecimal(account_data["current_holdings"]) # Current amount of stock held in account
 
-        self.streamer = self.client.stream # create streamer
+        # Start streaming data from Schwab
+        self.streamer = self.client.stream # Create streamer
 
-    # buy sell hold
+    # buy sell hold (to be updated by classes that inherit Trader)
     def bsh_decision(self):
         pass
 
     def bsh(self, decision):
+        # If we're holding don't do anything
         if decision["action"] == "hold":
             self.last_action = decision["action"]
             return
 
-        if "price" in decision.keys():
-            price = TwoDecimal(decision["price"])
-            quantity = TwoDecimal(decision["quantity"])
+        price = TwoDecimal(decision["price"])
+        quantity = TwoDecimal(decision["quantity"])
 
         # assuming our order gets filled at market price
         if self.debug:
@@ -63,6 +64,7 @@ class Trader():
                 self.current_holdings = quantity
                 self.account_cash = self.account_cash - (price * quantity)
             elif decision["action"] == "sell":
+                # profit is equal to the price * quantity - last_buy price * quantity
                 self.session_profit = self.session_profit + (price * quantity) - (self.last_action_price * quantity)
                 self.last_action_price = price
                 self.current_holdings = TwoDecimal("0")
@@ -80,7 +82,7 @@ class Trader():
                     sleep(1)
                     status = self.api_handler.get_order_status(order_id)
                 
-                # make updates as necessary
+                # make updates as necessary (only need to update profit when selling)
                 fill_price = TwoDecimal(status["fill_price"])
                 if decision["action"] == "sell":
                     self.session_profit = self.session_profit + (fill_price * quantity) - (self.last_action_price * quantity)
@@ -96,8 +98,7 @@ class Trader():
             self.account_cash = TwoDecimal(updates["account_balance"])
             self.current_holdings = TwoDecimal(updates["current_holdings"])
             # Everytime we buy or sell, save our state in a bucket for loading
-            if not self.debug:
-                self.save_state("trader-bucket-61423", "state.json", "state.json")
+            self.save_state("trader-bucket-61423", "state.json", "state.json")
 
         self.last_action = decision["action"]
         self.system_message = f"Last Action: {self.last_action.upper()}, Price: {self.last_action_price}"
@@ -111,7 +112,6 @@ class Trader():
             # 1 - bid and 2 - ask. Market price = bid + ask / 2. Or just use last price (3)
             if "1" in content.keys() and "2" in content.keys() and "3" in content.keys(): 
                 market_price = (TwoDecimal(str(content["1"])) + TwoDecimal(str(content["2"]))) / 2
-                logging.info(market_price)
 
                 self.market_price = market_price
                 # start up the trader
@@ -161,6 +161,8 @@ class Trader():
         # Now load data from file
         with open("state.json", "r") as file:
             data = json.load(file)
+        
+        # Need to make sure bot knows last buy/sell price
         self.last_action_price = TwoDecimal(data["last_action_price"])
 
     def save_state(self, bucket_name, source_file_name, destination_blob_name):
@@ -173,6 +175,11 @@ class Trader():
         
         # Create a blob (object in the bucket)
         blob = bucket.blob(destination_blob_name)
+
+        # Create file to store
+        data = self.get_trader_data() # has some extra data but good enough for now
+        with open("state.json", "r") as file:
+            json.dump(data, file)
         
         # Upload the file
         blob.upload_from_filename(source_file_name)
