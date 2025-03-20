c2c422d22549073d3e2ec78671e57e6af2ec29fe
Sam <samgfales@gmail.com>
2025-03-19 18:34:36 -0600

    got rid of old stuff

--- local_testing_example/get_tokens.py
+++
@@ -1,7 +0,0 @@
-# Run this script when you need to generate a tokens.json file
-import schwabdev
-
-app_key = "APP KEY HERE"
-secret_key = "SECRET KEY HERE"
-
-client = schwabdev.Client(app_key, secret_key)  #create a client
--- local_testing_example/local_test.sh
+++
@@ -1,6 +0,0 @@
-docker build -t algo-trader .
-docker run -p 8080:8080 \
-    -e APP_KEY="APP KEY HERE" \
-    -e SECRET_KEY="SECRET KEY HERE" \
-    -e TOKENS='TOKENS HERE' \
-    algo-trader
